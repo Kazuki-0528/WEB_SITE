a7954ea360a64d15d97d3e961c1b9b659afa3508
github raamenzurururu <reku9696@gmail.com>
2021-01-29 19:47:10 +0900

    [add]アコーディオンパネルの実装

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-$(function () {});
+$(function () {
+  // アコーディオンパネル
+  $("dd").css("display", "none");
+  $("dl dt").click(function () {
+    if ($("+dd", this).css("display") == "none") {
+      $("+dd", this).slideDown("slow");
+      $(this).addClass("selected");
+    } else {
+      $("+dd", this).slideUp("slow");
+      $(this).removeClass("selected");
+    }
+  });
+});
