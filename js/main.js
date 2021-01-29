cc90b18f8192835b9aa6cfa22cea5ae8d213cc41
github raamenzurururu <reku9696@gmail.com>
2021-01-29 16:40:15 +0900

    [add]page-top.btn削除

--- js/main.js
+++ js/main.js
@@ -1,15 +1,1 @@
-$(function () {
-  $(".page-top").hide();
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 100) {
-      $(".page-top").fadeIn();
-    } else {
-      $(".page-top").fadeOut();
-    }
-  });
-
-  $(".page-top").click(function () {
-    $("body,html").animate({ scrollTop: 0 }, 500);
-    return false;
-  });
-});
+$(function () {});
